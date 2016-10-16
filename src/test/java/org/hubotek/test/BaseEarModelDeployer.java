8dce978e994d46766327a23cc56bae39d80cd718
Darth Vader <jose.carlos.canova@gmail.com>
2016-10-16 15:44:53 -0300

    Updated EAR test using http-client

--- src/test/java/org/hubotek/test/BaseEarModelDeployer.java
+++ src/test/java/org/hubotek/test/BaseEarModelDeployer.java
@@ -5,17 +5,36 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-import javax.ejb.EJB;
-import javax.inject.Inject;
-
-import org.hubotek.server.ejb.ServiceFacade;
-import org.hubotek.server.ejb.SessionFacadeServer;
-import org.hubotek.service.Service;
+import org.hubotek.ElementEnum;
+import org.hubotek.model.HubDocument;
+import org.hubotek.model.atom.AtomBase;
+import org.hubotek.model.feed.FeedUrl;
+import org.hubotek.model.google.GoogleBase;
+import org.hubotek.model.google.cse.GoogleSearchEngine;
+import org.hubotek.model.google.news.NewsTopic;
+import org.hubotek.model.lob.AtomDocumentContent;
+import org.hubotek.model.lob.GoogleResultItem;
+import org.hubotek.model.lob.RssItemDescription;
+import org.hubotek.model.project.api.GoogleApiKey;
+import org.hubotek.model.rss.RssDocument;
+import org.hubotek.model.search.GoogleSearchResult;
+import org.hubotek.model.url.NamedUrl;
+import org.hubotek.service.DataBaseService;
+import org.hubotek.service.data.RssDocumentService;
+import org.hubotek.service.ejb.HttpService;
+import org.hubotek.service.ejb.HubDocumentService;
+import org.hubotek.service.ejb.LocalService;
+import org.hubotek.service.ejb.document.HubDocumentServiceImpl;
+import org.hubotek.service.ejb.document.HubDocumentType;
+import org.hubotek.service.ejb.http.HttpServiceImpl;
 import org.hubotek.service.http.HttpRequestParameters;
 import org.hubotek.service.http.HttpRequestProcessor;
+import org.hubotek.service.http.RequestReference;
 import org.hubotek.service.http.RequestType;
+import org.hubotek.service.orm.PersistenceService;
+import org.hubotek.test.http.TestHttpRequestProcessor;
+import org.hubotek.util.DOMElementExtratorUtil;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
@@ -23,20 +42,9 @@ import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.nanotek.Base;
 
-
-@RunWith(Arquillian.class)
 public class BaseEarModelDeployer {
-
-	@EJB
-	private ServiceFacade someEjb;
-
-	@Inject
-	private HttpRequestProcessor requestProcessor;
-
 
 	@Deployment
 	public static Archive<?> createDeploymentPackage() throws IOException {
@@ -47,38 +55,53 @@ public class BaseEarModelDeployer {
 		Stream<File> jarFilesStream = Arrays.asList(files).stream();//.forEach(s -> print(s.getName()))
 
 		final JavaArchive ejbJar = ShrinkWrap.create(JavaArchive.class, "ejb-jar.jar")
-				.addClass(ServiceFacade.class)
-				.addClass(SessionFacadeServer.class)
+				.addPackage(FeedUrl.class.getPackage())
+				.addPackage(AtomDocumentContent.class.getPackage())
+				.addPackage(AtomBase.class.getPackage())
+				.addPackage(BaseModelPersistenceTestClass.class.getPackage())
+				.addPackage(Base.class.getPackage())
+				.addPackage(GoogleApiKey.class.getPackage())
+				.addPackage(DOMElementExtratorUtil.class.getPackage())
+				.addPackage(ElementEnum.class.getPackage())
+				.addPackage(HubDocument.class.getPackage())
+				.addPackage(RssDocument.class.getPackage())
+				.addPackage(RssItemDescription.class.getPackage())
+				.addPackage(NamedUrl.class.getPackage())
+				.addPackage(GoogleSearchEngine.class.getPackage())
+				.addPackage(GoogleBase.class.getPackage())
+				.addPackage(NewsTopic.class.getPackage())
+				.addPackage(GoogleResultItem.class.getPackage())
+				.addPackage(GoogleSearchResult.class.getPackage())
+				.addPackage(DataBaseService.class.getPackage())
+				.addPackage(RssDocumentService.class.getPackage())
+				.addPackage(PersistenceService.class.getPackage())
+				.addPackage(HttpServiceImpl.class.getPackage())
+				.addClass(HubDocumentService.class)
+				.addClass(HubDocumentServiceImpl.class)
+				.addClass(LocalService.class)
+				.addClass(HttpService.class)
+				.addClass(HttpServiceImpl.class)
+				.addClass(HttpRequestProcessor.class)
+				.addClass(HubDocumentType.class)
+				.addClass(RequestType.class)
+				.addClass(RequestReference.class)
+				.addClass(HttpRequestParameters.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+				.addAsResource("log4j.properties", "log4j.properties");
+
+		/*final JavaArchive ejbJar = ShrinkWrap.create(JavaArchive.class, "ejb-jar.jar")
 				.addPackage(Service.class.getPackage())
 				.addPackage(HttpRequestProcessor.class.getPackage())
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");*/
 
-		// Embedding war package which contains the test class is needed
-		// So that Arquillian can invoke test class through its servlet test runner
-		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war").addClass(BaseEarModelDeployer.class);
+		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war").addClass(BaseEarModelDeployer.class).addClass(TestHttpRequestProcessor.class);
 		final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class)
 				.setApplicationXML("ear/test-application.xml")
 				.addAsModule(ejbJar)
 				.addAsModule(testWar);
 		jarFilesStream.forEach(jf -> ear.addAsLibraries(jf));
 		return ear;
-	}
-
-	/*private static void print(String value) {
-			System.err.println(value);
-		}*/
-
-	@Test
-	public void test() {
-		Assert.assertEquals("Hello, Kyle", someEjb.hello("Kyle"));
-	}
-	
-	@Test
-	public void test_http_call()
-	{ 
-		String response = requestProcessor.processRequest("https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&output=rss", new HttpRequestParameters() , RequestType.GET);
-		Assert.assertNotNull(response);
-		System.err.println(response);
 	}
 
 }
